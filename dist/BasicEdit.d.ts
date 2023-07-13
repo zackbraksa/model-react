7d83593249f784828e6f36d01761aa47ff5ec455
Alex Milenkovic <jk1143298@gmail.com>
2023-07-13 16:41:30 +0200

    dist

---
+++ dist/BasicEdit.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare function BasicEdit(props: any): JSX.Element;
+export default BasicEdit;
---
+++ dist/BasicList.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare function BasicList(props: any): JSX.Element;
+export default BasicList;
