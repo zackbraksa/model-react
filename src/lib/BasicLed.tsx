a5a11aa827b640982a45398fbbfb4478b6fc39ae
Alex Milenkovic <jk1143298@gmail.com>
2023-07-25 18:32:06 +0200

    status field

--- src/lib/BasicLed.tsx
+++ src/lib/BasicLed.tsx
@@ -69,12 +69,12 @@ function BasicLed(props: any) {
   const columns = 
     itemFields.map((field: any) => 
       ({
-        accessorFn: (row: any) => row[field.field],
+        accessorFn: (row: any) => ( 'status' === field.type ? field.options[row[field.field]]?.title : row[field.field] ),
         accessorKey: field.field,
         header: field.headerName,
         Header: () => <span>{ field.headerName }</span>,
         // muiTableHeadCellProps: { sx: { color: 'green' } },
-        Cell: ({ cell }: any) => <span>{cell.getValue()}</span>,
+        Cell: ({ cell }: any) => <span>{ cell.getValue() }</span>,
       })
     )
   
