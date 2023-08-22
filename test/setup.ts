6c127e7de66e7754c4a5423d7bf3118a64399dd6
Ríona Stokes <rionastokes@gmail.com>
2023-08-22 19:14:23 +0100

    Add test setup and initial truthy/falsy tests

---
+++ test/setup.ts
@@ -0,0 +1,11 @@
+import { expect, afterEach } from 'vitest'
+import { cleanup } from '@testing-library/react'
+import matchers from '@testing-library/jest-dom/matchers'
+
+// extend vite's expect method with matchers from react test lib
+expect.extend(matchers)
+
+// cleanup after each test case (clearing jsdom etc)
+afterEach(() => {
+  cleanup()
+})
