eba7324dafac780d6b0444d4089a13cfcf50aaf6
Ríona Stokes <rionastokes@gmail.com>
2023-08-24 18:03:21 +0100

    Further troubleshooting

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 import * as React from 'react'
 import { render, screen } from '@testing-library/react'
 
-import { BasicDrawerHeader } from '../src/lib/BasicDrawer'
+import { BasicDrawer } from '../src/lib/BasicDrawer'
 
 describe('component-testing', () => {
   // test tests
@@ -13,14 +13,9 @@ describe('component-testing', () => {
     expect(false).toBe(false)
   })
 
-  // Cannot render Vxg
-  // Dependant components: BasicAdmin, BasicAuth, BasicEdit, BasicHead, BasicLed, BasicMain, BasicSide
-  // Error with rendering BasicList, BasicFoot: `ctx is not a function`
-
-  // Failing `unable to find element with this text` - what am I looking for?
   // it('render-basic-drawer-header', () => {
-  //   render(<BasicDrawerHeader />)
+  //   render(<BasicDrawer />)
 
-  //   expect(screen.getByText(/BasicDrawerHeader/)).toBeDefined()
+  //   expect(screen.getByText(/BasicDrawer/)).toBeDefined()
   // })
 })
