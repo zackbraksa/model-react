27ac2d2a3f8d5310304b9cf3601d031baecf2f59
Ríona Stokes <rionastokes@gmail.com>
2023-08-30 17:41:54 +0100

    Successful render tests and wip mocking of others

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -2,78 +2,140 @@ import { describe, it, expect } from 'vitest'
 import * as React from 'react'
 import { render, screen } from '@testing-library/react'
 
-// import BasicAdmin from '../src/lib/BasicAdmin'
-// import BasicAppBar from '../src/lib/BasicAppBar'
-// import BasicAuth from '../src/lib/BasicAuth'
-// import BasicButton from '../src/lib/BasicButton'
-// import { BasicDrawer } from '../src/lib/BasicDrawer'
-// import { BasicDrawerHeader } from '../src/lib/BasicDrawer'
-// import BasicEdit from '../src/lib/BasicEdit'
-// import BasicFoot from '../src/lib/BasicFoot'
-// import BasicHead from '../src/lib/BasicHead'
-// import BasicLed from '../src/lib/BasicLed'
-// import BasicList from '../src/lib/BasicList'
-// import BasicMain from '../src/lib/BasicMain'
-// import BasicSide from '../src/lib/BasicSide'
 import {
+  BasicAdmin,
   BasicAppBar,
+  BasicAuth,
   BasicButton,
   BasicDrawer,
   BasicDrawerHeader,
+  BasicEdit,
+  BasicFoot,
+  BasicHead,
+  BasicLed,
+  BasicList,
+  BasicMain,
+  BasicSide,
 } from '../src/lib/index'
-// import Vxg from '../src/lib/vxg'
 
 describe('render-component', () => {
   // it('render-basic-admin', () => {
-  //   // ctx is not a function
-  //   render(<BasicAdmin />)
+  //   // useNavigate() may be used only in the context of a <Router> component
+  //   let ctx = () => {
+  //     return {
+  //       model: {
+  //         app: { web: { frame: { frame: { part: { foot: 'foot' } } } } },
+  //       },
+  //     }
+  //   }
+  //   let spec = { frame: 'frame' }
+  //   render(<BasicAdmin ctx={ctx} spec={spec} />)
   // })
+
   it('render-basic-appbar', () => {
     render(<BasicAppBar />)
-    screen.debug()
+    // screen.debug()
   })
-  // it('render-basic-auth', () => {
-  //   // Cannot destructure property 'handle' of 'spec' as it is undefined.
-  //   render(<BasicAuth />)
-  // })
+
+  it('render-basic-auth', () => {
+    let ctx = () => {}
+    let spec = { img: { logo: 'logo' }, handle: { signin: () => {} } }
+    render(<BasicAuth ctx={ctx} spec={spec} />)
+  })
+
   it('render-basic-button', () => {
     render(<BasicButton />)
-    screen.debug()
+    // screen.debug()
   })
+
   it('render-basic-drawer', () => {
     render(<BasicDrawer />)
-    screen.debug()
+    // screen.debug()
   })
+
   it('render-basic-drawer-header', () => {
     render(<BasicDrawerHeader />)
-    screen.debug()
+    // screen.debug()
   })
+
   // it('render-basic-edit', () => {
-  //   // ctx is not a function
-  //   render(<BasicEdit />)
+  //   // vitest thinks .map() is an attribute and not a function
+  //   let ctx = () => {
+  //     return { model: 'model', seneca: 'seneca', custom: 'custom' }
+  //   }
+  //   let spec = {
+  //     content: { def: 'def' },
+  //     itemFields: { field1: { name: 'name' }, field2: { name: 'name' } },
+  //   }
+  //   render(<BasicEdit ctx={ctx} spec={spec} />)
   // })
-  // it('render-basic-foot', () => {
-  //   // ctx is not a function
-  //   render(<BasicFoot />)
-  // })
+
+  it('render-basic-foot', () => {
+    let ctx = () => {
+      return {
+        model: {
+          app: { web: { frame: { frame: { part: { foot: 'foot' } } } } },
+        },
+      }
+    }
+    let spec = { frame: 'frame' }
+    render(<BasicFoot ctx={ctx} spec={spec} />)
+  })
+
   // it('render-basic-head', () => {
-  //   // ctx is not a function
-  //   render(<BasicHead />)
+  //   // useNavigate() may be used only in the context of a <Router> component
+  //   let ctx = () => {
+  //     return { model: 'model', seneca: 'seneca' }
+  //   }
+  //   let spec = {}
+  //   render(<BasicHead ctx={ctx} spec={spec} />)
   // })
+
   // it('render-basic-led', () => {
-  //   // ctx is not a function
-  //   render(<BasicLed />)
+  //   // Could not find react-redux context value; please ensure the component is wrapped in a <Provider>
+  //   let ctx = () => {
+  //     return { model: 'model', seneca: 'seneca', custom: 'custom' }
+  //   }
+  //   let spec = {}
+  //   render(<BasicLed ctx={ctx} spec={spec} />)
   // })
+
   // it('render-basic-list', () => {
-  //   // ctx is not a function
-  //   render(<BasicList />)
+  //   // Could not find react-redux context value; please ensure the component is wrapped in a <Provider>
+  //   let ctx = () => {
+  //     return { model: 'model', seneca: 'seneca', custom: 'custom' }
+  //   }
+  //   let spec = {}
+  //   render(<BasicList ctx={ctx} spec={spec} />)
   // })
+
   // it('render-basic-main', () => {
-  //   // ctx is not a function
-  //   render(<BasicMain />)
+  //   // Could not find react-redux context value; please ensure the component is wrapped in a <Provider>
+  //   let ctx = () => {
+  //     return {
+  //       model: {
+  //         app: {
+  //           web: {
+  //             frame: {
+  //               frame: { part: { foot: 'foot' } },
+  //               private: { view: 'view' },
+  //             },
+  //           },
+  //         },
+  //       },
+  //       content: 'content',
+  //     }
+  //   }
+  //   let spec = { frame: 'frame' }
+  //   render(<BasicMain ctx={ctx} spec={spec} />)
   // })
+
   // it('render-basic-side', () => {
-  //   // ctx is not a function
-  //   render(<BasicSide />)
+  //   // Could not find react-redux context value; please ensure the component is wrapped in a <Provider>
+  //   let ctx = () => {
+  //     return { model: 'model', seneca: 'senca' }
+  //   }
+  //   let spec = {}
+  //   render(<BasicSide ctx={ctx} spec={spec} />)
   // })
 })
