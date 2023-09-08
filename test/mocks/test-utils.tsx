efb985f1d95bffcb28ede95445997b0384e42a1d
Ríona Stokes <rionastokes@gmail.com>
2023-09-08 19:28:37 +0100

    Remove comments

--- test/mocks/test-utils.tsx
+++ test/mocks/test-utils.tsx
@@ -7,54 +7,6 @@ import { Provider } from 'react-redux'
 import { setupStore } from './store'
 import type { AppStore, RootState } from './store'
 
-/*
-// MEDIUM
-// test-utils.jsx
-import React from 'react'
-import { render as rtlRender } from '@testing-library/react'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-// Import the character
-import userSlice from '../features/user/state/userSlice'
-//import the characterSlice
-import characterSlice from '../features/character/characterSlice'
-import campaignSlice from '../features/campaign/campaignSlice'
-
-function render(
-  ui,
-  {
-    preloadedState,
-    store = configureStore({
-      reducer: {
-        user: userSlice,
-        //use the characterSlice within the store
-        character: characterSlice,
-        campaign: campaignSlice,
-      },
-      preloadedState,
-    }),
-    route = '/',
-    ...renderOptions
-  } = {}
-) {
-  window.history.pushState({}, 'Test Page', route)
-  function Wrapper({ children }) {
-    return (
-      <BrowserRouter>
-        <Provider store={store}>{children}</Provider>
-      </BrowserRouter>
-    )
-  }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions })
-}
-
-// re-export everything
-export * from '@testing-library/react'
-// override render method
-export { render }
-*/
-
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as initialState, store.
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
