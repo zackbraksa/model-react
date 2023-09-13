e0eb2be692f2580a906aa832ef07f0ce20aa7eb1
Ríona Stokes <rionastokes@gmail.com>
2023-09-13 18:36:11 +0100

    Add more mocking variables to ctx

--- test/BasicAdmin.test.tsx
+++ test/BasicAdmin.test.tsx
@@ -1,5 +1,33 @@
 import { describe, it } from 'vitest'
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { BasicAdmin } from '../src/lib/index'
 
 describe('BasicAdmin', () => {
-  it('happy', () => {})
+  it('happy', () => {
+    let ctx = () => {
+      return {
+        model: {
+          app: {
+            web: {
+              frame: {
+                frame: {
+                  part: { foot: 'foot', head: { tool: { def: {} } } },
+                },
+              },
+            },
+          },
+        },
+      }
+    }
+    let spec = { frame: 'frame' }
+
+    render(
+      <BrowserRouter>
+        <BasicAdmin ctx={ctx} spec={spec} />
+      </BrowserRouter>
+    )
+  })
 })
