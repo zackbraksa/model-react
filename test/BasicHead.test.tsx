2b4ac24c996e39ce947cfde1ff84dc529407ae34
Ríona Stokes <rionastokes@gmail.com>
2023-09-13 18:38:14 +0100

    Add back in happy test in progress

--- test/BasicHead.test.tsx
+++ test/BasicHead.test.tsx
@@ -1,5 +1,32 @@
 import { describe, it } from 'vitest'
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+
+import { renderWithProviders } from './mocks/test-utils'
+import { BasicHead } from '../src/lib/index'
 
 describe('BasicHead', () => {
-  it('happy', () => {})
+  it('happy', () => {
+    let ctx = () => {
+      return {
+        model: {
+          app: {
+            web: {
+              frame: { frame: { part: { head: { tool: { def: {} } } } } },
+            },
+          },
+        },
+        seneca: 'seneca',
+      }
+    }
+    let spec = { frame: 'frame' }
+
+    renderWithProviders(<BasicHead ctx={ctx} spec={spec} />, {
+      wrapper: BrowserRouter,
+      preloadedState: {
+        main: { auth: { user: 'user' } },
+      },
+    })
+  })
 })
