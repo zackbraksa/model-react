014ace287c7dba242f71e687679bdb20d08fdaec
Ríona Stokes <rionastokes@gmail.com>
2023-09-15 17:05:09 +0100

    BasicMain happy success w stderr no routes matched

--- test/BasicMain.test.tsx
+++ test/BasicMain.test.tsx
@@ -1,5 +1,31 @@
 import { describe, it } from 'vitest'
+import * as React from 'react'
+
+import { customRender } from './mocks/test-utils'
+import { BasicMain } from '../src/lib/index'
 
 describe('BasicMain', () => {
-  it('happy', () => {})
+  it('happy', () => {
+    let ctx = () => {
+      return {
+        model: {
+          app: {
+            web: {
+              frame: {
+                frame: { part: { foot: 'foot' } },
+                private: { view: [{ name: 'view1' }] },
+              },
+            },
+          },
+        },
+        content: 'content',
+      }
+    }
+    let spec = { frame: 'frame' }
+    let initialState = { main: { vxg: { cmp: { BasicSide: {} } } } }
+
+    customRender(<BasicMain ctx={ctx} spec={spec} />, {
+      mockInitialState: initialState,
+    })
+  })
 })
