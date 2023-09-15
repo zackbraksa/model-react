4c55c009e89b56d63f49aefcaf2785dbcaeebbaf
Ríona Stokes <rionastokes@gmail.com>
2023-09-15 16:55:20 +0100

    Success happy test BasicLed

--- test/BasicLed.test.tsx
+++ test/BasicLed.test.tsx
@@ -1,5 +1,29 @@
 import { describe, it } from 'vitest'
+import * as React from 'react'
+
+import { customRender } from './mocks/test-utils'
+import { BasicLed } from '../src/lib/index'
 
 describe('BasicLed', () => {
-  it('happy', () => {})
+  it('happy', () => {
+    let ctx = () => {
+      return { model: 'model', seneca: 'seneca', custom: 'custom' }
+    }
+    let spec = { content: { def: { ent: { canon: 'canon' } } } }
+    let initialState = {
+      main: {
+        vxg: {
+          ent: {
+            meta: { main: { canon: { state: {} } } },
+            list: { main: { canon: {} } },
+          },
+          trigger: { led: { add: {} } },
+        },
+      },
+    }
+
+    customRender(<BasicLed ctx={ctx} spec={spec} />, {
+      mockInitialState: initialState,
+    })
+  })
 })
