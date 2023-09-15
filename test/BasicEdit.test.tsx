7db5ac27e119c1a34b3b2ca768d290455ce963d1
Ríona Stokes <rionastokes@gmail.com>
2023-09-15 16:49:13 +0100

    BasicEdit happy test success

--- test/BasicEdit.test.tsx
+++ test/BasicEdit.test.tsx
@@ -1,5 +1,19 @@
 import { describe, it } from 'vitest'
+import * as React from 'react'
+import { render } from '@testing-library/react'
+
+import { BasicEdit } from '../src/lib/index'
 
 describe('BasicEdit', () => {
-  it('happy', () => {})
+  it('happy', () => {
+    let ctx = () => {
+      return { model: 'model', seneca: 'seneca', custom: 'custom' }
+    }
+    let spec = {
+      content: { def: 'def' },
+    }
+    let itemFields = []
+
+    render(<BasicEdit ctx={ctx} spec={spec} itemFields={itemFields} />)
+  })
 })
