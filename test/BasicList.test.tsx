acab22ef25235a5219370c1412b11839da85e06f
Ríona Stokes <rionastokes@gmail.com>
2023-09-19 18:01:02 +0100

    Move wip out of PR branch

--- test/BasicList.test.tsx
+++ test/BasicList.test.tsx
@@ -1,19 +1,5 @@
 import { describe, it } from 'vitest'
-import * as React from 'react'
-
-import { customRender } from './mocks/test-utils'
-import { BasicList } from '../src/lib/index'
 
 describe('BasicList', () => {
-  it('happy', () => {
-    let ctx = () => {
-      return { model: 'model', seneca: 'seneca', custom: 'custom' }
-    }
-    let spec = {}
-    let initialState = { main: { vxg: {} } }
-
-    customRender(<BasicList ctx={ctx} spec={spec} />, {
-      mockInitialState: initialState,
-    })
-  })
+  it('happy', () => {})
 })
