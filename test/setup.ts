3d573e04210d9ca2ab94d96461dea7aa2430f481
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-04 17:00:58 +0100

    Comment out a line that makes tests fail

--- test/setup.ts
+++ test/setup.ts
@@ -3,7 +3,7 @@ import { cleanup } from '@testing-library/react'
 import matchers from '@testing-library/jest-dom/matchers'
 
 // extend vite's expect method with matchers from react test lib
-expect.extend(matchers)
+// expect.extend(matchers);
 
 // cleanup after each test case (clearing jsdom etc)
 afterEach(() => {
