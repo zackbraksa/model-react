f5071d06d5d5c49f8ffe760a0c6f718a7edb841f
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-04 17:12:43 +0100

    Fix error: Cannot convert undefined or null to object

--- test/setup.ts
+++ test/setup.ts
@@ -1,9 +1,9 @@
 import { expect, afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
-import matchers from '@testing-library/jest-dom/matchers'
+import * as matchers from '@testing-library/jest-dom/matchers'
 
 // extend vite's expect method with matchers from react test lib
-// expect.extend(matchers);
+expect.extend(matchers)
 
 // cleanup after each test case (clearing jsdom etc)
 afterEach(() => {
