431a1c8bf0261f47c97b3d44e70705035856602e
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-11 00:27:25 +0100

    Fix BasicMain width and left padding

--- src/lib/BasicMain.tsx
+++ src/lib/BasicMain.tsx
@@ -10,21 +10,21 @@ import {
 
 
 import BasicLed from './BasicLed'
-  
+
 
 function makeCmp(view: any, ctx: any) {
-  let cmp: any = ()=><div>NONE</div>
+  let cmp: any = () => <div>NONE</div>
 
   const content = view.content || {}
 
-  if('custom' === content.kind) {
+  if ('custom' === content.kind) {
     cmp = ctx().cmp[content.cmp]
   }
-  else if('led' === content.kind) {
+  else if ('led' === content.kind) {
     cmp = BasicLed
   }
 
-  return cmp 
+  return cmp
 }
 
 
@@ -36,58 +36,61 @@ function BasicMain(props: any) {
     spec,
   } = props
   const { model, content } = ctx()
-  
+
   const { frame } = spec
-  
+
   const part = model.app.web.frame[frame].part.main
 
   const views = Object.values(model.app.web.frame.private.view)
-  
+
   const sideOpen = useSelector((state: any) => state.main.vxg.cmp.BasicSide.show)
-  
-  
+
+
   const divStyle = {
     'paddingLeft': sideOpen ? '12.0em' : '0em',
     'paddingRight': 0,
   }
   const mainDiv = {
-    height: 'calc(100vh - 6rem)',
-    width: sideOpen ? 'calc(100vw - 19rem)' : 'calc(100vw - 4rem)',
-    padding: '84px ' + (sideOpen ? '4.5em' : '0') + ' 4.5em ' + (sideOpen ? '4.5em' : '0.5em')
+    // height: 'calc(100vh - 6rem)',
+    // width: sideOpen ? 'calc(100vw - 19rem)' : 'calc(100vw - 4rem)',
+    // padding: '84px ' + (sideOpen ? '4.5em' : '0') + ' 4.5em ' + (sideOpen ? '4.5em' : '0.5em')
+    width: sideOpen ? 'calc(100% - 16rem)' : '100%',
+    paddingLeft: sideOpen ? '16rem' : '1rem',
+    paddingTop: '7rem',
   }
-  
+
   return (
-    <div className="BasicMain" style = { mainDiv }>
-      <div style={{width:'100%',height:'100%', ...divStyle }}>
+    <div className="BasicMain" style={mainDiv}>
+      <div style={{ width: '100%', height: '100%', ...divStyle }}>
         <Routes>
           <Route path="/view">
             {
-              views.map((view:any) => {
-                const Cmp:any = makeCmp(view, ctx)
-                if(view.paramId) {
+              views.map((view: any) => {
+                const Cmp: any = makeCmp(view, ctx)
+                if (view.paramId) {
                   return (
                     <Fragment key={view.name}>
                       <Route
                         key={view.name}
-                        path={ '/view/'+view.name }
-                        element={<Cmp vxg={vxg} ctx={ctx} spec={view}/>}
+                        path={'/view/' + view.name}
+                        element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
                       />
                       <Route
                         key={view.name}
-                        path={ '/view/' + view.name + '/:' + view.paramId }
-                        element={<Cmp vxg={vxg} ctx={ctx} spec={view}/>}
-                      />   
+                        path={'/view/' + view.name + '/:' + view.paramId}
+                        element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
+                      />
                     </Fragment>)
                 }
                 return <Route
                   key={view.name}
-                  path={'/view/'+view.name}
-                  element={<Cmp vxg={vxg} ctx={ctx} spec={view}/>}
+                  path={'/view/' + view.name}
+                  element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
                 />
               })
             }
-        </Route>
-      </Routes>
+          </Route>
+        </Routes>
       </div>
     </div>
   )
