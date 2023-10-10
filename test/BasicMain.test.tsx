483f07c6ac90ada73e6325e5a50cadeb77fb9021
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-10 23:21:40 +0100

    Refactor model to spec in BasicMain

--- test/BasicMain.test.tsx
+++ test/BasicMain.test.tsx
@@ -1,12 +1,27 @@
 import { describe, it } from 'vitest'
 import * as React from 'react'
 
-import { customRender, ctx, spec, initialState } from './mocks/test-utils'
+import { customRender, ctx, spec, initialState, vxg } from './mocks/test-utils'
 import { BasicMain } from '../src/lib/index'
 
 describe('BasicMain', () => {
   it('happy', () => {
-    customRender(<BasicMain ctx={ctx} spec={spec} />, {
+
+    const mainSpec = {
+      main: {},
+      view: {
+        task: {
+          content: {
+            def: {
+              add: {}
+            }
+          },
+          name: 'task'
+        }
+      }
+    }
+
+    customRender(<BasicMain vxg={vxg} ctx={ctx} spec={mainSpec} />, {
       mockInitialState: initialState,
     })
   })
