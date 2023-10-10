be02cd1d37119a19396ebb56d030e27fffef6faa
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-10 23:22:01 +0100

    Refactor model to spec in BasicSide

--- src/lib/BasicSide.tsx
+++ src/lib/BasicSide.tsx
@@ -30,11 +30,11 @@ import {
   ChevronLeft,
   MoveToInbox as InboxIcon,
   Mail as MailIcon,
-  
+
   FactoryOutlined,
   KeyOutlined,
   AssignmentTurnedInOutlined,
-  TextSnippetOutlined,  
+  TextSnippetOutlined,
   HighlightAlt,
   SupervisorAccount as AccountIcon,
   Map as MapIcon,
@@ -85,11 +85,11 @@ function onClose(seneca: any) {
 }
 
 function allow(vxg: any, item: any) {
-  let out = (item && item.allow) ? vxg.allow( item.allow ) : true
+  let out = (item && item.allow) ? vxg.allow(item.allow) : true
   return out
 }
-    
-    
+
+
 function BasicSide(props: any) {
   const {
     vxg,
@@ -100,41 +100,41 @@ function BasicSide(props: any) {
 
   const vxgState = useSelector((state: any) => state.main.vxg)
   const open = vxgState.cmp.BasicSide.show
-  
-  
-  
+
+
+
   const navigate = useNavigate()
   const location = useLocation()
-  
-  
-  const { frame } = spec
-  
-  const part = model.app.web.frame[frame].part.side
 
-  const viewmap = model.app.web.frame[frame].view
+
+  const { frame } = spec
+
+  const part = spec.side
+
+  const viewmap = spec.view
   const viewdefs = Object.entries(viewmap)
-    .map((entry:any)=>(entry[1].name=entry[0],entry[1]))
+    .map((entry: any) => (entry[1].name = entry[0], entry[1]))
 
   const sectiondefs = Object.entries(part.section || [])
-    .map((entry:any)=>(entry[1].name=entry[0],entry[1]))
-  
+    .map((entry: any) => (entry[1].name = entry[0], entry[1]))
+
   const viewPath: any = location.pathname.split('/')[2]
-  
+
   const [showViewsData, setShowViewsData] = useState(sectiondefs.map((section: any, sectionNumber: number) => {
     return viewPath == section.name || (section.view && viewPath in section.view)
   }))
-  const [toogleSelections, setToogleSelections] = useState({[viewPath]: true} as any)
-  
+  const [toogleSelections, setToogleSelections] = useState({ [viewPath]: true } as any)
+
   const drawerwidth = '16rem'
 
-  function selectView(view:any) {
-    return function(_event:any) {
+  function selectView(view: any) {
+    return function (_event: any) {
       // TODO: use named route
-      if(view.default) {
-        navigate('/view/'+view.default)
+      if (view.default) {
+        navigate('/view/' + view.default)
         return
       }
-      navigate('/view/'+view.name)
+      navigate('/view/' + view.name)
     }
   }
 
@@ -145,12 +145,12 @@ function BasicSide(props: any) {
     // remove not prevously valid views
     return orderedViews.filter((view) => view !== undefined)
   }
-  
+
 
   function toggle(sectionNumber: any) {
-    return function(_event: any) {
+    return function (_event: any) {
       setShowViewsData((showViewsData: any) => {
-        const temp = showViewsData.map((_ : Boolean) => false)
+        const temp = showViewsData.map((_: Boolean) => false)
         temp[sectionNumber] = true
         return temp
       })
@@ -162,66 +162,66 @@ function BasicSide(props: any) {
     const { viewdefs, viewOrder } = props
     return (
       <Box sx={{ overflow: 'auto' }}>
-       <ToggleButtonGroup
-        orientation="vertical"
-        aria-label="text alignment"
-        sx={{ width: '100%' }}
-      >
-        {
-          sortViews(viewdefs, viewOrder).map((view:any) => (
-           allow(vxg, view) ? <ToggleButton
-              value="check"
-              selected={ viewPath == view.name }
-              sx={
-                {
-                  width: '100%',
-                  display: 'flex',
-                  justifyContent: 'flex-start',
-                  marginBottom: '10px',
-                  border: 0,
-                  '&.MuiToggleButtonGroup-grouped': {
-                    borderRadius: '20px !important',
-                  },
-                  textTransform: 'none'
-                }
-              } 
-              key={view.name}
-              aria-label="centered"
-              onClick={(event: any) => {
-                /*
-                setToogleSelections((prev: any)=>{
-                  for(let name in prev) {
-                    prev[name] = false
+        <ToggleButtonGroup
+          orientation="vertical"
+          aria-label="text alignment"
+          sx={{ width: '100%' }}
+        >
+          {
+            sortViews(viewdefs, viewOrder).map((view: any) => (
+              allow(vxg, view) ? <ToggleButton
+                value="check"
+                selected={viewPath == view.name}
+                sx={
+                  {
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'flex-start',
+                    marginBottom: '10px',
+                    border: 0,
+                    '&.MuiToggleButtonGroup-grouped': {
+                      borderRadius: '20px !important',
+                    },
+                    textTransform: 'none'
                   }
-                  prev[view.name] = true
-                  return prev
-                })
-                */
-                selectView(view)(event)
-              }}
-             >
-                
-               { makeIcon(view.icon) }
-               <div>
-                 <span>{ view.title }</span>
-               </div>
-             </ToggleButton> : null
-          ))
-        }  
-       </ToggleButtonGroup>
-     </Box>
+                }
+                key={view.name}
+                aria-label="centered"
+                onClick={(event: any) => {
+                  /*
+                  setToogleSelections((prev: any)=>{
+                    for(let name in prev) {
+                      prev[name] = false
+                    }
+                    prev[view.name] = true
+                    return prev
+                  })
+                  */
+                  selectView(view)(event)
+                }}
+              >
+
+                {makeIcon(view.icon)}
+                <div>
+                  <span>{view.title}</span>
+                </div>
+              </ToggleButton> : null
+            ))
+          }
+        </ToggleButtonGroup>
+      </Box>
     )
   }
 
   const SectionButtons = (props: any) => {
     const { sections } = props
-    if(sections.length === 1) {
+    if (sections.length === 1) {
       return null
     }
 
     return (
-      <Box sx= {{ display: 'flex', width: '100%' }}>
-        <ButtonGroup sx={{ width: '100%',}}>
+      <Box sx={{ display: 'flex', width: '100%' }}>
+        <ButtonGroup sx={{ width: '100%', }}>
           {
             sections.map((section: any, sectionNumber: number) => (
               <ToggleButton
@@ -230,7 +230,7 @@ function BasicSide(props: any) {
                 sx={{
                   padding: '0.5em',
                   width: '100%',
-                }} 
+                }}
                 key={section.name}
                 onChange={(event: any) => {
                   toggle(sectionNumber)(event)
@@ -238,8 +238,8 @@ function BasicSide(props: any) {
                 }}
               >
                 <div>
-                  { makeIcon(section.button.icon) }
-                  <span className="iconText">{ section.button.text }</span>
+                  {makeIcon(section.button.icon)}
+                  <span className="iconText">{section.button.text}</span>
                 </div>
               </ToggleButton>
             ))}
@@ -251,38 +251,38 @@ function BasicSide(props: any) {
   return (
     <BasicDrawer
       variant="permanent"
-      drawerwidth = { drawerwidth }
-      open={ open }
+      drawerwidth={drawerwidth}
+      open={open}
     >
       <BasicDrawerHeader>
         <img src={part.logo.img}
-          style={ { width: '11rem' } } />
-        <IconButton onClick={()=>onClose(seneca)}>
+          style={{ width: '11rem' }} />
+        <IconButton onClick={() => onClose(seneca)}>
           <ChevronLeft sx={{ color: 'black' }} />
         </IconButton>
       </BasicDrawerHeader>
-        
-        
-      <SectionButtons sections={ sectiondefs }/>
-        {
-          sectiondefs.map((section: any, sectionNumber: number) => {
-            if(viewPath == section.name || (section.view && viewPath in section.view) ) {
-      
-              if('navmenu' === section.kind) {
-                return (
-                  <DefaultNavMenu key={section.name} viewOrder={section.view} viewdefs={viewdefs}/>
-                )
-              }
 
-              const Cmp:any = ctx().cmp[section.cmp]
+
+      <SectionButtons sections={sectiondefs} />
+      {
+        sectiondefs.map((section: any, sectionNumber: number) => {
+          if (viewPath == section.name || (section.view && viewPath in section.view)) {
+
+            if ('navmenu' === section.kind) {
               return (
-                <Cmp key={section.name} ctx={ctx} spec={spec}/>
+                <DefaultNavMenu key={section.name} viewOrder={section.view} viewdefs={viewdefs} />
               )
             }
-          })
-        }
+
+            const Cmp: any = ctx().cmp[section.cmp]
+            return (
+              <Cmp key={section.name} ctx={ctx} spec={spec} />
+            )
+          }
+        })
+      }
     </BasicDrawer>
-    )
+  )
 }
 
 
