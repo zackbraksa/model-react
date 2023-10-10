c3ebcbce1e837553d47690b91aafef0752ef7306
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-10 18:45:16 +0100

    Added hooks to BasicLed

--- test/BasicLed.test.tsx
+++ test/BasicLed.test.tsx
@@ -1,13 +1,42 @@
-import { describe, it } from 'vitest'
+import { describe, it, beforeEach, afterEach } from 'vitest'
 import * as React from 'react'
 
-import { customRender, ctx, spec, initialState } from './mocks/test-utils'
+import { customRender, ctx, spec, initialState, vxg } from './mocks/test-utils'
 import { BasicLed } from '../src/lib/index'
+
+beforeEach(() => {
+  Object.defineProperty(window, 'location', {
+    value: {
+      pathname: '/view/task'
+    }
+  });
+});
 
 describe('BasicLed', () => {
   it('happy', () => {
-    customRender(<BasicLed ctx={ctx} spec={spec} />, {
+
+    const view = {
+      content: {
+        def: {
+          ent: {
+            canon: 'vxg/task'
+          },
+          add: {}
+        }
+      },
+      name: 'task'
+    }
+
+    customRender(<BasicLed vxg={vxg} ctx={ctx} spec={view} />, {
       mockInitialState: initialState,
     })
   })
 })
+
+afterEach(() => {
+  Object.defineProperty(window, 'location', {
+    value: {
+      pathname: window.location.pathname // Restore to the original value
+    }
+  });
+});
